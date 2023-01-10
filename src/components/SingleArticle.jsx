5b267b72208032ac9953401118c5bfb9e0b905a7
Varsha Elsa Jacob <varsha@Varshas-MacBook-Air.local>
2023-01-10 10:31:04 +0000

    this commit invovles: navigating to a article page once clicked on required article. The article page includes article details with vote comment section (funcationality not added)

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -6,20 +6,37 @@ const SingleArticle = () => {
 
     const {article_id} = useParams()
     const [articleDisplay, setArticleDisplay] = useState([])
+    const [isLoading,setIsLoading] = useState(true)
 
     useEffect(() => {
+        setIsLoading(true)
         getArticleById(article_id).then(res => {
             setArticleDisplay(res.article)
+            setIsLoading(false)
         })
     },[article_id])
 
+    if (isLoading) {
+        return <p>Loading ...</p>
+    }
+
     return (
         <div>
-            <h2>{articleDisplay.title}</h2>
-            <p>Author: {articleDisplay.author}</p>
-            <p>{articleDisplay.created_at} </p>
-            <p>{articleDisplay.body}</p>
-            <button className="VoteButton"><img src='https://static.thenounproject.com/png/343589-200.png' className="VoteButton"></img></button><button className="VoteButton"><img src='https://thenounproject.com/api/private/icons/4190940/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0&token=gAAAAABjvEOFx2e2Pe3ZUukY1S-TBJFP06W0vLseWJGfdzNDKcv9Ui80o6hl63IQrTdDulWYs2SpzM180kP1dG4fgaRJlFYR4w%3D%3D' className="VoteButton"></img></button><button className="VoteCount">{articleDisplay.votes}</button>
+            <section className="ArticleDetails">
+            <h2 className='ArticleTitle'>{articleDisplay.title}</h2>
+            <p className='ArticleAuthor'>Author: {articleDisplay.author}</p>
+            <p className='ArticleDate'>{articleDisplay.created_at} </p>
+            <p className='ArticleBody'>{articleDisplay.body}</p>
+            <p className='ArticleVotes'>Votes: {articleDisplay.votes}</p>
+            </section>
+            <section className="VoteSection">
+            <button className="UpVoteButton"></button>
+            <button className="DownVoteButton"></button>
+            </section>
+            <section className="VoteSection">
+            <button className="CommentButton">Comment</button>
+            </section>
+            
         </div>
        
     )
